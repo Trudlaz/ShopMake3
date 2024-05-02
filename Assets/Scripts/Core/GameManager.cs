57c5bdefaab598012785659627ccce5541fdb697
ReaverMan <reaverdrop50@gmail.com>
2024-05-02 09:45:21 +0900

    통합 후 커밋

--- Assets/Scripts/Core/GameManager.cs
+++ Assets/Scripts/Core/GameManager.cs
@@ -4,34 +4,32 @@ public class GameManager : Singleton<GameManager>
     private Player player;
     private ItemDataManager itemDataManager;
     private Inventory_UI inventoryUI;
-    private WorldInventory worldInventory;
     private WorldInventory_UI worldInventoryUI;
     private TimeSystem timeSys;
     private WeaponBase weaponBase;
     private ShopInventoryUI shopInventoryUI; // 샵 인벤토리 UI 추가
+    private Equip_UI equipUI;
 
     // 플레이어, 아이템 데이터 관리자, 여러 인벤토리 UI 속성 정의
     public Player Player => player;
     public ItemDataManager ItemData => itemDataManager;
     public Inventory_UI InventoryUI => inventoryUI;
-    public WorldInventory WorldInventory => worldInventory;
     public WorldInventory_UI WorldInventory_UI => worldInventoryUI;
     public TimeSystem TimeSystem => timeSys;
     public WeaponBase WeaponBase => weaponBase;
     public ShopInventoryUI ShopInventoryUI => shopInventoryUI; // 샵 인벤토리 UI에 대한 프로퍼티 추가
+    public Equip_UI EquipUI => equipUI;
 
     // 초기화 단계에서 플레이어, 아이템 데이터 관리자, 인벤토리 UI 초기화
     protected override void OnInitialize()
     {
         player = FindAnyObjectByType<Player>();
-        
         inventoryUI = FindAnyObjectByType<Inventory_UI>();
         worldInventoryUI = FindAnyObjectByType<WorldInventory_UI>();
-        // WorldInventory 객체 생성
-        worldInventory = new WorldInventory(player);
         timeSys = FindAnyObjectByType<TimeSystem>();
         weaponBase = FindAnyObjectByType<WeaponBase>();
         shopInventoryUI = FindAnyObjectByType<ShopInventoryUI>(); // 샵 인벤토리 UI 초기화
+        equipUI = FindObjectOfType<Equip_UI>();
     }
 
     // 아이템 데이터 관리자 초기화 전에 호출되는 초기화 단계
--- Assets/Scripts/Item/UseItem/ItemData/ItemDataManager.cs
+++ Assets/Scripts/Item/UseItem/ItemData/ItemDataManager.cs
@@ -8,34 +8,16 @@ public class ItemDataManager : MonoBehaviour
     public ItemData[] items = null;
 
     /// <summary>
-    /// 아이템 코드를 이용하여 아이템 데이터를 안전하게 가져옵니다.
+    /// 
     /// </summary>
-    /// <param name="code">아이템 코드</param>
-    /// <param name="itemData">찾아낸 아이템 데이터를 출력합니다.</param>
-    /// <returns>데이터가 있으면 true, 없으면 false</returns>
-    public bool TryGetItemData(ItemCode code, out ItemData itemData)
-    {
-        int index = (int)code;
-        if (index >= 0 && index < items.Length)
-        {
-            itemData = items[index];
-            return true;
-        }
-        itemData = null;
-        return false;
-    }
-
-    /// <summary>
-    /// 아이템 코드를 이용하여 아이템 데이터를 직접 반환합니다.
-    /// </summary>
-    /// <param name="type">아이템 코드</param>
-    /// <returns>해당 코드의 아이템 데이터</returns>
+    /// <param name="type"></param>
+    /// <returns></returns>
     public ItemData this[ItemCode type] => items[(int)type];
 
     /// <summary>
-    /// 인덱스를 이용하여 아이템 데이터를 직접 반환합니다.
+    /// 아이템의 개수?
     /// </summary>
-    /// <param name="index">인덱스</param>
-    /// <returns>해당 인덱스의 아이템 데이터</returns>
+    /// <param name="index"></param>
+    /// <returns></returns>
     public ItemData this[int index] => items[index];
 }
